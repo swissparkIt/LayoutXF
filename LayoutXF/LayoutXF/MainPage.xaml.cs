35be92139dd8b4c6a5f87653a96cd2ced1d55681
julio <juliodisouza@gmail.com>
2019-02-26 23:53:54 -0300

    Adicionar arquivos de projeto.

---
+++ LayoutXF/LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         <Button Text="Stack" Clicked="GoPaginaStack" ></Button>
+        <Button Text="Grid" Clicked="GoPaginaGrid" ></Button>
+        <Button Text="Absolute" Clicked="GoPaginaAbsolute" ></Button>
+        <Button Text="Relative" Clicked="GoPaginaRelative" ></Button>
+        <Button Text="Scroll" Clicked="GoPaginaScroll" ></Button>
+        */
+
+        private void GoPaginaStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+
+        private void GoPaginaGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+
+        private void GoPaginaAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+
+        private void GoPaginaRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+
+        private void GoPaginaScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+
+    }
+}
